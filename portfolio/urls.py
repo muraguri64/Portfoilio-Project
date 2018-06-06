c7ff0591f73acbd4f6ee1766c3242932a1ce43b0
muraguri64 <muraguri64@gmail.com>
2018-06-06 18:56:29 +0300

    Add signup, login and logout accounts

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,5 +24,7 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name="alljobs"),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls'))
+
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
