99f929c4c71b5cddb87dab35334d2e361e88626e
muraguri64 <muraguri64@gmail.com>
2018-06-07 21:41:07 +0300

    Add create url in blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from  . import views
 urlpatterns = [
 
     path('', views.allblogs, name="allblogs"),
+    path('create/', views.create, name="create_blog"),
     path('<int:blog_id>/',views.details, name="blog_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
 
 from .models import Blog
+
 
 # Create your views here.
 
@@ -11,3 +15,21 @@ def allblogs(request):
 def details(request, blog_id):
     detail_blog = get_object_or_404(Blog, pk=blog_id)
     return render(request,'blog/detail.html',{'blog':detail_blog})
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        if request.POST['title'] and request.POST['body'] and request.FILES['image']:
+            blog = Blog()
+            blog.title    = request.POST['title']
+            blog.image    = request.FILES['image']
+            blog.body     = request.POST['body']
+            blog.pub_date = timezone.datetime.now()
+
+            blog.save()
+            return redirect('allblogs')
+        else:
+             return render(request,'blog/create.html',{'error':'All fields are required'})
+
+    else:
+        return render(request,'blog/create.html')
