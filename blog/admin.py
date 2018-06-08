272502a865769becab72514012b6c57630a53997
muraguri64 <muraguri64@gmail.com>
2018-06-08 19:23:19 +0300

    Add More columns to jobs and blog admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,10 @@ from django.contrib import admin
 from .models import Blog
 # Register your models here.
 
-admin.site.register(Blog)
+class blogInfoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body_summary', 'pub_date')
+
+    def body_summary(self,obj):
+        return obj.body[:50] + "..."
+
+admin.site.register(Blog, blogInfoAdmin)
--- jobs/admin.py
+++ jobs/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Job
 # Register your models here.
 
-admin.site.register(Job)
+class jobsInfoAdmin(admin.ModelAdmin):
+    list_display = ('summary','image')
+
+
+
+admin.site.register(Job,jobsInfoAdmin)
