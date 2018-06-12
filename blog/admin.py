4d0a91cb1f0a0dc3743be6ff2879cb6702d7563f
muraguri64 <muraguri64@gmail.com>
2018-06-12 14:54:30 +0300

    Add more filters to Blog Admin

--- blog/admin.py
+++ blog/admin.py
@@ -6,9 +6,13 @@ from .models import Blog
 
 class blogInfoAdmin(admin.ModelAdmin):
     list_display = ('title', 'body_summary', 'pub_date')
+    list_filter = ('title', 'pub_date')
+    search_fields = ('title', 'body')
 
     def body_summary(self,obj):
         return obj.body[:50] + "..."
 
     body_summary.short_description = 'Summary'
+
+
 admin.site.register(Blog, blogInfoAdmin)
